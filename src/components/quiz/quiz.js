08e1cc71943e99ebfc248a48e47f59b5464c8aed
Khomko Dima <37930691+Khomko-Dima@users.noreply.github.com>
2021-01-14 17:41:42 +0300

    u

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import { rippleEffect } from "../main"
 
 export default () => {
+  const [state, setState] = useState(1);
+  const [value, setValue] = useState({
+    site1: null,
+    site2: null,
+    site3: null,
+    site4: null
+  })
+
   // const [quizData, setQuizData] = useState({
   //   name: {
   //     value: "",
@@ -25,17 +33,97 @@ export default () => {
 
   const handleQuiz = (e, direction) => {
     rippleEffect(e)
-    // console.log("следующая страница")
-  }
+    if (direction == 'prev') {
+      setState(state - 1)
+    } else {setState(state + 1)}
 
-  return (
-    <form name="quiz" className="p-3 col-6 border-top">
+  }
+  const handleCheckbox = (e) => {
+    if (e.target.tagName !== 'INPUT') return
+    const val = e.target.value;
+    switch (e.target.name) {
+      case 'site1':
+        setValue({
+          ...value,
+          site1: val
+        })
+        break;
+      case 'site2':
+        setValue({
+          ...value,
+          site2: val
+        })
+        break;
+      case 'site3':
+        setValue({
+          ...value,
+          site3: val
+        })
+        break;
+      case 'site4':
+        setValue({
+          ...value,
+          site4: val
+        })
+        break;
+    }
+  }
+  console.log(value)
+
+  const firtsSection = (
+    <div className="d-flex flex-column">
+      Какой тип сайта вас интересует?
+      <div className="checkbox mt-3">
+        <input
+          id="CV"
+          name="site1"
+          type="radio"
+          checked={}
+          value="Сайт-визитка"/>
+        <label htmlFor="CV">Сайт-визитка</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="EC"
+          name="site1"
+          type="radio"
+          value="Интернет-магазин"/>
+        <label htmlFor="EC">Интернет-магазин</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="LP"
+          name="site1"
+          type="radio"
+          value="Продающая страница"/>
+        <label htmlFor="LP">Продающая страница</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="CS"
+          name="site1"
+          type="radio"
+          value="Корпоративный сайт"/>
+        <label htmlFor="CS">Корпоративный сайт</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="OS"
+          name="site1"
+          type="radio"
+          value="Онлайн-сервис"/>
+        <label htmlFor="OS">Онлайн-сервис</label>
+      </div>
+    </div>
+  )
+
+  const secondSection = (
       <div className="d-flex flex-column">
-        Какой тип сайта вас интересует?
+        Какой функционал необходимо реализовать на сайте?
         <div className="checkbox mt-3">
           <input
             id="CV"
-            name="site"
+            name="site2"
             type="radio"
             value="Сайт-визитка"/>
           <label htmlFor="CV">Сайт-визитка</label>
@@ -43,7 +131,7 @@ export default () => {
         <div className="checkbox">
           <input
             id="EC"
-            name="site"
+            name="site2"
             type="radio"
             value="Интернет-магазин"/>
           <label htmlFor="EC">Интернет-магазин</label>
@@ -51,7 +139,7 @@ export default () => {
         <div className="checkbox">
           <input
             id="LP"
-            name="site"
+            name="site2"
             type="radio"
             value="Продающая страница"/>
           <label htmlFor="LP">Продающая страница</label>
@@ -59,7 +147,7 @@ export default () => {
         <div className="checkbox">
           <input
             id="CS"
-            name="site"
+            name="site2"
             type="radio"
             value="Корпоративный сайт"/>
           <label htmlFor="CS">Корпоративный сайт</label>
@@ -67,151 +155,119 @@ export default () => {
         <div className="checkbox">
           <input
             id="OS"
-            name="site"
+            name="site2"
             type="radio"
             value="Онлайн-сервис"/>
           <label htmlFor="OS">Онлайн-сервис</label>
         </div>
-        <button type="button" className="mainBtn" onClick={(e) => handleQuiz(e, "next")}>Далее</button>
       </div>
+  )
 
-      {/*<div className="d-flex flex-column">*/}
-      {/*  Какой функционал необходимо реализовать на сайте?*/}
-      {/*  <div className="checkbox mt-3">*/}
-      {/*    <input*/}
-      {/*      id="CV"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Сайт-визитка"/>*/}
-      {/*    <label htmlFor="CV">Сайт-визитка</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="EC"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Интернет-магазин"/>*/}
-      {/*    <label htmlFor="EC">Интернет-магазин</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="LP"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Продающая страница"/>*/}
-      {/*    <label htmlFor="LP">Продающая страница</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="CS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Корпоративный сайт"/>*/}
-      {/*    <label htmlFor="CS">Корпоративный сайт</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="OS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Онлайн-сервис"/>*/}
-      {/*    <label htmlFor="OS">Онлайн-сервис</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="d-flex justify-content-between">*/}
-      {/*    <button type="button" className="mainBtn" onClick={(e) => handleQuiz(e, "prev")}>Назад</button>*/}
-      {/*    <button type="button" className="mainBtn" onClick={(e) => handleQuiz(e, "next")}>Далее</button>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+  const thirdSection = (
+    <div className="d-flex flex-column">
+      Какой тип сайта вас интересует?
+      <div className="checkbox mt-3">
+        <input
+          id="CV"
+          name="site3"
+          type="radio"
+          value="Сайт-визитка"/>
+        <label htmlFor="CV">Сайт-визитка</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="EC"
+          name="site3"
+          type="radio"
+          value="Интернет-магазин"/>
+        <label htmlFor="EC">Интернет-магазин</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="LP"
+          name="site3"
+          type="radio"
+          value="Продающая страница"/>
+        <label htmlFor="LP">Продающая страница</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="CS"
+          name="site3"
+          type="radio"
+          value="Корпоративный сайт"/>
+        <label htmlFor="CS">Корпоративный сайт</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="OS"
+          name="site3"
+          type="radio"
+          value="Онлайн-сервис"/>
+        <label htmlFor="OS">Онлайн-сервис</label>
+      </div>
+    </div>
+  )
+  const fourthSection = (
+    <div className="d-flex flex-column">
+      Какой тип сайта вас интересует?
+      <div className="checkbox mt-3">
+        <input
+          id="CV"
+          name="site4"
+          type="radio"
+          value="Сайт-визитка"/>
+        <label htmlFor="CV">Сайт-визитка</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="EC"
+          name="site4"
+          type="radio"
+          value="Интернет-магазин"/>
+        <label htmlFor="EC">Интернет-магазин</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="LP"
+          name="site4"
+          type="radio"
+          value="Продающая страница"/>
+        <label htmlFor="LP">Продающая страница</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="CS"
+          name="site4"
+          type="radio"
+          value="Корпоративный сайт"/>
+        <label htmlFor="CS">Корпоративный сайт</label>
+      </div>
+      <div className="checkbox">
+        <input
+          id="OS"
+          name="site4"
+          type="radio"
+          value="Онлайн-сервис"/>
+        <label htmlFor="OS">Онлайн-сервис</label>
+      </div>
+    </div>
+  )
+  return (
+    <form name="quiz" className="p-3 col-6 border-top" onClick={e => handleCheckbox(e)}>
+      {state === 1 ? firtsSection : null}
+      {state === 2 ? secondSection : null}
+      {state === 3 ? thirdSection : null}
+      {state === 4 ? fourthSection : null}
 
-      {/*<div className="d-flex flex-column">*/}
-      {/*  Какой тип сайта вас интересует?*/}
-      {/*  <div className="checkbox mt-3">*/}
-      {/*    <input*/}
-      {/*      id="CV"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Сайт-визитка"/>*/}
-      {/*    <label htmlFor="CV">Сайт-визитка</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="EC"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Интернет-магазин"/>*/}
-      {/*    <label htmlFor="EC">Интернет-магазин</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="LP"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Продающая страница"/>*/}
-      {/*    <label htmlFor="LP">Продающая страница</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="CS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Корпоративный сайт"/>*/}
-      {/*    <label htmlFor="CS">Корпоративный сайт</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="OS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Онлайн-сервис"/>*/}
-      {/*    <label htmlFor="OS">Онлайн-сервис</label>*/}
-      {/*  </div>*/}
-      {/*  <button type="button" className="mainBtn" onClick={handleQuiz}>Далее</button>*/}
-      {/*</div>*/}
-
-      {/*<div className="d-flex flex-column">*/}
-      {/*  Какой тип сайта вас интересует?*/}
-      {/*  <div className="checkbox mt-3">*/}
-      {/*    <input*/}
-      {/*      id="CV"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Сайт-визитка"/>*/}
-      {/*    <label htmlFor="CV">Сайт-визитка</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="EC"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Интернет-магазин"/>*/}
-      {/*    <label htmlFor="EC">Интернет-магазин</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="LP"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Продающая страница"/>*/}
-      {/*    <label htmlFor="LP">Продающая страница</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="CS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Корпоративный сайт"/>*/}
-      {/*    <label htmlFor="CS">Корпоративный сайт</label>*/}
-      {/*  </div>*/}
-      {/*  <div className="checkbox">*/}
-      {/*    <input*/}
-      {/*      id="OS"*/}
-      {/*      name="site"*/}
-      {/*      type="radio"*/}
-      {/*      value="Онлайн-сервис"/>*/}
-      {/*    <label htmlFor="OS">Онлайн-сервис</label>*/}
-      {/*  </div>*/}
-      {/*  <button type="button" className="mainBtn" onClick={handleQuiz}>Далее</button>*/}
-      {/*</div>*/}
+      <div className="d-flex justify-content-between">
+        {state === 1 ? null : (
+          <button type="button" className="mainBtn" onClick={(e) => handleQuiz(e, "prev")}>Назад</button>
+        )}
+        {state === 4 ? null : (
+          <button type="button" className="mainBtn" onClick={(e) => handleQuiz(e, "next")}>Далее</button>
+        )}
+      </div>
 
     </form>
   )
