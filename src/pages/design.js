69e8d2f11c279536e11205dc0ed08c29ed716621
Semantisby <info@semantis.by>
2021-01-22 16:37:09 +0300

    fix bug with document

--- src/pages/design.js
+++ src/pages/design.js
@@ -11,25 +11,26 @@ import Logo from "../components/design-page/logo"
 import Contacts from "../components/home-page/contacts"
 
 
-const DesignPage = ({location}) => {
+const DesignPage = ({ location }) => {
   const [sectionNumber, setSectionNumber] = useState(0)
   const handleSectionChange = number => {
     if (sectionNumber !== number) {
       setSectionNumber(number)
     }
   }
-
-  if (document.documentElement.clientWidth <= 991) {
-    return (
-      <Layout location={location}>
-        <SEO title="Разработка сайтов и веб-приложений"/>
-        <FirstSectionDesign/>
-        <Identity/>
-        <Presentations/>
-        <Logo/>
-        <Contacts/>
-      </Layout>
-    )
+  if (typeof window !== `undefined`) {
+    if (/Android|webOS|Mac OS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || document.documentElement.clientWidth <= 991) {
+      return (
+        <Layout location={location}>
+          <SEO title="Разработка сайтов и веб-приложений"/>
+          <FirstSectionDesign/>
+          <Identity/>
+          <Presentations/>
+          <Logo/>
+          <Contacts/>
+        </Layout>
+      )
+    }
   } else return (
     <Layout selectedSection={sectionNumber} location={location}>
       <SEO title="Разработка сайтов и веб-приложений"/>
